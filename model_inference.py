19729b0844753701d30e5b8896ee3da8d2d55552
DmitriyVahrushev <notmybestmail@yandex.ru>
2021-02-14 17:45:48 +0300

    simple frequences instead of bert

--- model_inference.py
+++ model_inference.py
@@ -1,15 +1,56 @@
-from summarizer import Summarizer
+import re
+import nltk
+import heapq
 
-model = Summarizer()
+nltk.download('punkt')
+nltk.download('stopwords')
+stopwords = nltk.corpus.stopwords.words('english')
 
 def predict(text):
-	result = model(text, ratio=0.2)
-	#full = ''.join(result)
-	orig_sentences = text.split(".") # use something smarter for this in the future
-	res = ''
-	for sentence in orig_sentences:
-		if sentence in result:
-			res += " <strong>" + sentence + "</strong>"
+	article_text = re.sub(r'\[[0-9]*\]', ' ', text)
+	article_text = re.sub(r'\s+', ' ', article_text)
+
+	formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
+	formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+
+	sentence_list = nltk.sent_tokenize(article_text)
+
+	word_frequencies = {}
+	for word in nltk.word_tokenize(formatted_article_text):
+		if word not in stopwords:
+			if word not in word_frequencies.keys():
+				word_frequencies[word] = 1
+			else:
+				word_frequencies[word] += 1
+
+	maximum_frequncy = max(word_frequencies.values())
+
+	for word in word_frequencies.keys():
+		word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+
+	maximum_frequncy = max(word_frequencies.values())
+
+	for word in word_frequencies.keys():
+		word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+
+	sentence_scores = {}
+
+	for sent in sentence_list:
+		for word in nltk.word_tokenize(sent.lower()):
+			if word in word_frequencies.keys():
+				if len(sent.split(' ')) < 30:
+					if sent not in sentence_scores.keys():
+						sentence_scores[sent] = word_frequencies[word]
+					else:
+						sentence_scores[sent] += word_frequencies[word]
+
+
+	summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+	#res = ' '.join(summary_sentences)
+	res = ' '
+	for sent in sentence_list:
+		if sent in summary_sentences:
+			res += " <strong>" + sent + "</strong>"
 		else:
-			res += " "+ sentence
+			res += " "+ sent
 	return res
