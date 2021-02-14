3e746e94dd30a5e0e2c5460bb0b205183c6c400d
DmitriyVahrushev <notmybestmail@yandex.ru>
2021-02-14 16:52:49 +0300

    add bert summarizer

--- app.py
+++ app.py
@@ -20,5 +20,21 @@ def upload_text():
 	return render_template('index.html')
 
 
+'''В POST-запросе должен быть json  с текстом. возвращает json c разметкой текста'''
+@app.route('/api/get_predict', methods=['POST'])
+def api_get_text():
+	if request.method == 'POST':
+		print(request.form)
+		if 'input_text' not in request.form:
+			return render_template('index.html', output_text = "Не было введено текста!")
+
+		input_text = request.form['input_text']
+		if not input_text:
+			return render_template('index.html', output_text = "Не было введено текста!")
+		output_text = predict(input_text)
+		return render_template('index.html', output_text = output_text)
+	return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
--- model_inference.py
+++ model_inference.py
@@ -1,4 +1,15 @@
+from summarizer import Summarizer
 
+model = Summarizer()
 
 def predict(text):
-	return "<strong>" + text + "</strong>"
+	result = model(text, ratio=0.2)
+	#full = ''.join(result)
+	orig_sentences = text.split(".") # use something smarter for this in the future
+	res = ''
+	for sentence in orig_sentences:
+		if sentence in result:
+			res += " <strong>" + sentence + "</strong>"
+		else:
+			res += " "+ sentence
+	return res
