02456fd768ae96e90670a7118228e631bc70b734
Ancirev Artem <ancyev_2727@list.ru>
2021-02-27 22:17:11 +0300

    Upgrade tf-idf model. Possible work with  tf and tf-idf models. Add support russian language.

--- model_inference.py
+++ model_inference.py
@@ -1,51 +1,86 @@
 import re
 import nltk
 import heapq
+import math
 
 nltk.download('punkt')
 nltk.download('stopwords')
-stopwords = nltk.corpus.stopwords.words('english')
+language = "russian"  # 'english'
+stopwords = nltk.corpus.stopwords.words(language)
 
-def predict(text):
-	article_text = re.sub(r'\[[0-9]*\]', ' ', text)
-	article_text = re.sub(r'\s+', ' ', article_text)
 
-	formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
-	formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
-
-	sentence_list = nltk.sent_tokenize(article_text)
-
+def compute_tf(article_text):
 	word_frequencies = {}
-	for word in nltk.word_tokenize(formatted_article_text):
+	for word in nltk.word_tokenize(article_text, language=language):
 		if word not in stopwords:
 			if word not in word_frequencies.keys():
 				word_frequencies[word] = 1
 			else:
 				word_frequencies[word] += 1
 
-	maximum_frequncy = max(word_frequencies.values())
+	maximum_frequency = max(word_frequencies.values())
 
 	for word in word_frequencies.keys():
-		word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+		word_frequencies[word] = (word_frequencies[word] / maximum_frequency)
 
-	sentence_scores = {}
+	return word_frequencies
 
+
+def compute_idf(sentence_list):
+	idf_measure = {}
+	for sent_number, sent in enumerate(sentence_list):
+		for word in nltk.word_tokenize(sent.lower(), language=language):
+			if len(sent.split(' ')) < 30:
+				if word not in idf_measure:
+					idf_measure[word] = [sent_number]
+				elif sent_number not in idf_measure[word]:
+					idf_measure[word].append(sent_number)
+
+	N = len(sentence_list)
+
+	for key, value in idf_measure.items():
+		idf_measure[key] = math.log(N / len(value))
+
+	return idf_measure
+
+
+def compute_tf_idf(article_text, idf_flag=True):
+	sentence_list = nltk.sent_tokenize(article_text, language=language)
+	tf_measure = compute_tf(article_text)
+	idf_measure = dict.fromkeys(tf_measure.keys(), 1)
+	if idf_flag:
+		idf_measure = compute_idf(sentence_list)
+
+	tf_idf_measure = {}
 	for sent in sentence_list:
-		for word in nltk.word_tokenize(sent.lower()):
-			if word in word_frequencies.keys():
+		for word in nltk.word_tokenize(sent.lower(), language=language):
+			if word in tf_measure.keys():
 				if len(sent.split(' ')) < 30:
-					if sent not in sentence_scores.keys():
-						sentence_scores[sent] = word_frequencies[word]
+					if sent not in tf_idf_measure.keys():
+						tf_idf_measure[sent] = tf_measure[word] * idf_measure[word]
 					else:
-						sentence_scores[sent] += word_frequencies[word]
+						tf_idf_measure[sent] += tf_measure[word] * idf_measure[word]
+
+	return  tf_idf_measure
 
 
-	summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
-	#res = ' '.join(summary_sentences)
+def predict(text):
+	article_text = re.sub(r'\[[0-9]*\]', ' ', text)
+	article_text = re.sub(r'\s+', ' ', article_text)
+
+	# formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
+	# formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+
+	sentence_list = nltk.sent_tokenize(article_text, language=language)
+	sentence_score = compute_tf_idf(article_text)
+
+	summary_sentences = heapq.nlargest(7, sentence_score, key= sentence_score.get)
+	# res = ' '.join(summary_sentences)
 	res = ' '
 	for sent in sentence_list:
 		if sent in summary_sentences:
 			res += " <strong>" + sent + "</strong>"
 		else:
-			res += " "+ sent
+			res += " " + sent
+
 	return res
